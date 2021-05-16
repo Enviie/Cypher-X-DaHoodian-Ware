b234678e5ead9ce0378ff6541f17f723f6e117c1
Enviie <50255493+Enviie@users.noreply.github.com>
2021-05-16 18:57:54 -0500

    Update Cypher X DaHoodian-Ware Main.lua

--- Cypher X DaHoodian-Ware Main.lua
+++ Cypher X DaHoodian-Ware Main.lua
@@ -1,4 +1,4 @@
-if syn or SENTINEL_V2 or pebc_create then
+if syn or SENTINEL_V2 or pebc_create or executescript then
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Enviie/Cypher-X-DaHoodian-Ware/main/Cypher%20X%20DaHoodian-Ware%20Paid%20Exploits.lua"))("Enviie Soo Hot!")
     wait()
     warn("Using Paid Exploits")
